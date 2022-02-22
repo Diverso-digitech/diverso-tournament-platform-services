9f8f7aaaaa8339425df9a3f12ff3ba4af53bc1b6
sabiqthottoly <sabiqt@gmail.com>
2022-02-22 23:51:06 +0530

    Add cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import db from './utils/db.js'
 import auth from './routes/auth.js'
+import cors from 'cors'
 
 const app = express()
 
 app.use('/', auth)
+app.use(cors())
 
-const server = app.listen(process.env.PORT || 8000, function () {
+const server = app.listen(process.env.PORT || 3000, function () {
     console.log('API server started on http://localhost:' + server.address().port)
 })
