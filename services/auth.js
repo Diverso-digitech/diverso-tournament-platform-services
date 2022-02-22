886669f831f39a6a1f786bb67762060659b9ff7b
sabiqthottoly <sabiqt@gmail.com>
2022-02-22 23:49:03 +0530

    function to register new user

--- services/auth.js
+++ services/auth.js
@@ -1,8 +1,30 @@
+import { Users } from "../models/index.js"
+import { handleError, handleResponse } from "../utils/common.js"
+
 async function registerUser (req,res) {
     try {
-        //auth function
+        const { mobile, token } = req.body
+        if (!mobile) {
+            handleError(res, 'Invalid Data', 'Invalid Data Packet Received', 400)
+        } else {
+			const alreadyExistingMobile = await Users.findOne({ mobile: mobile }).select({ mobile: 1 }).lean()
+            if (alreadyExistingMobile) {
+                console.log(`Dupicate entry, ${mobile} mobile number already exists`)
+				console.log('Existing user details')
+				return handleError(res, 'Duplicate Entry', 'Mobile Number Aleady Exists', 409)
+            } else {
+                const newUserObject = {
+                    mobile,
+                    joiningDate: new Date(),
+                }
+                const NewUserDocument = new Users(newUserObject)
+                const newUser = await NewUserDocument.save()
+				handleResponse(res, { exists: false }, "User created successfully", 200)
+            }
+        }       
     } catch (err) {
-
+        console.log(err)
+        return handleError(res, 'Server error', 'Server error while registering user', 500)
     }
 }
 
