771308dc9197986f0de2eacd546d3633a8c04172
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2022-03-08 23:28:50 +0530

    created formatted response creater

--- utils/common.js
+++ utils/common.js
@@ -1,17 +1,29 @@
 // Generic error handler used by all endpoints.
 export const handleError = (res, reason, message, code) => {
-	console.log('ERROR: ' + reason)
-	res.status(code || 500).json({
-		message: message,
-		reason: reason,
-		code: code
-	})
-}
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({
+    message: message,
+    reason: reason,
+    code: code,
+  });
+};
 
 export const handleResponse = (res, data, message, code) => {
-	res.status(code || 500).json({
-		message: message,
-		data: data,
-		code: code
-	})
-}
+  res.status(code || 500).json({
+    message: message,
+    data: data,
+    code: code,
+  });
+};
+
+export const formatResponse = (res, code, data, err, message) => {
+  res.json({
+    data,
+    meta: {
+      status: err ? false : true,
+      error: Boolean(err),
+      messageID: 1,
+      message,
+    },
+  });
+};
