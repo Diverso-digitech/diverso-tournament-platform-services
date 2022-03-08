f8143c4da925a5e480a7939d98c81df23923e507
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2022-03-08 23:29:22 +0530

    constants file

---
+++ constants/messages.js
@@ -0,0 +1,12 @@
+export const MESSAGES = {
+  SUCCESS: "SUCCESS",
+  NOT_AUTHORIZED: "NOT AUTHORIZED",
+  SERVER_ERROR: "SERVER ERROR",
+  REQUIRED_DATA: "REQUIRED_DATA",
+};
+
+export const NAMES = {
+  JWT_COOKIE: "JWT_COOKIE",
+};
+
+export const JWT_MAX_AGE = 28 * 24 * 60 * 60 * 1000; //28 days
