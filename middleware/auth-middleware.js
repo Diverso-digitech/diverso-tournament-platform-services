20977625ae367d9a01a25c858e53686a60f49769
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2022-03-08 23:30:19 +0530

    auth middlewares

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -1,19 +1,47 @@
 import { admin } from "../config/firebase-config.js";
+import { Users } from "../models/user.js";
+import jwt from "jsonwebtoken";
+import { AUTH_SECRET } from "../env.js";
+import { formatResponse } from "../utils/common.js";
+import { MESSAGES, NAMES } from "../constants/messages.js";
 
 class MiddlewareClass {
-  async decodeToken(req, res, next) {
-    const { token } = req.body;
-    try {
-      const decodeValue = await admin.auth().verifyIdToken(token);
-      if (decodeValue) {
-        req.user = decodeValue;
-        return next();
-      }
-      return res.json({ message: "Not Authorized" });
-    } catch (e) {
-      return res.json({ message: "Internal Error" });
+  requireAuth = (req, res, next) => {
+    const token = req.cookies[NAMES.JWT_COOKIE];
+    if (token) {
+      jwt.verify(token, AUTH_SECRET, (err, decodedToken) => {
+        console.log(err);
+        if (err) {
+          formatResponse(res, 501, {}, true, MESSAGES.NOT_AUTHORIZED);
+        } else {
+          next();
+        }
+      });
+    } else {
+      formatResponse(res, 501, {}, true, MESSAGES.NOT_AUTHORIZED);
     }
-  }
+  };
+
+  // check current user
+  checkUser = (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+      jwt.verify(token, AUTH_SECRET, async (err, decodedToken) => {
+        if (err) {
+          res.locals.user = null;
+          next();
+        } else {
+          let user = await Users.findById(decodedToken.id);
+          res.locals.user = user;
+          next();
+        }
+      });
+    } else {
+      res.locals.user = null;
+      next();
+    }
+  };
 }
 
 export { MiddlewareClass };
