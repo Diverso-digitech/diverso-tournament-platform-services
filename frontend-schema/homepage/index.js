179cf1ff03a5c0756c61494a2b83cc5e01dce2a0
GAURAVC710 <shashankm.malviya@gmail.com>
2022-04-01 23:27:26 +0530

    Revert "homepage schema apis"
    This reverts commit 700dfad2d08f62dd7bc258a8674ea18625276fc3.

--- frontend-schema/homepage/index.js
+++ frontend-schema/homepage/index.js
@@ -1,17 +1,12 @@
-import {
-  chipSectionDefaultItems,
-  CHIPS_SWIPER_PROPS,
-  defaultItems,
-  images,
-  SWIPER_PROPS,
-} from "./constants/homepageConstants.js";
+import { defaultItems } from "./constants/homepageConstants.js";
 
-export const homepageSchemaGenerator = (items) => {
+export const homepageSchemaGenerator = (items, numberOfElements = 10) => {
   return {
     schema: {
       sections: items.map(({ heading, type }) => ({
         heading,
         type,
+        data: Array.from(new Array(numberOfElements), () => ({})),
       })),
     },
   };
@@ -26,45 +21,5 @@ export const homepageSchema = (req, res) => {
       messageID: 1,
     },
     schema: homepageSchemaGenerator(defaultItems),
-  });
-};
-
-export const homepageChipSchemaGenerator = (items) => {
-  return items.map(({ gameName }) => ({
-    gameName,
-  }));
-};
-
-export const homepageChipsSchema = (req, res) => {
-  res.json({
-    data: {},
-    meta: {
-      status: true,
-      error: false,
-      messageID: 1,
-    },
-    schema: homepageChipSchemaGenerator(chipSectionDefaultItems),
-    swiperPropsChips: CHIPS_SWIPER_PROPS,
-  });
-};
-
-export const homepageTrendingGamesSchemaGenerator = (items) => {
-  return items.map(({ gameName }) => ({
-    gameName,
-    buttonName: "See Tournament...",
-    sampleImage: gameName === "Bgmi" ? images.PUBG : images.FREEFIRE,
-  }));
-};
-
-export const homepageTrendingGamesSchema = (req, res) => {
-  res.json({
-    data: {},
-    meta: {
-      status: true,
-      error: false,
-      messageID: 1,
-    },
-    schema: homepageTrendingGamesSchemaGenerator(chipSectionDefaultItems),
-    swiperProps: SWIPER_PROPS,
   });
 };
--- routes/schema.js
+++ routes/schema.js
@@ -1,22 +1,10 @@
 import { Router } from "express";
 import { API } from "../constants/apiConstants.js";
-import {
-  homepageChipsSchema,
-  homepageSchema,
-  homepageTrendingGamesSchema,
-} from "../frontend-schema/homepage/index.js";
+import { homepageSchema } from "../frontend-schema/homepage/index.js";
 const schemaRoutes = Router();
 
 schemaRoutes.get(API.HOME_PAGE_SCHEMA, (req, res) => {
   homepageSchema(req, res);
-});
-
-schemaRoutes.get(API.HOME_PAGE_CHIPS_SCHEMA, (req, res) => {
-  homepageChipsSchema(req, res);
-});
-
-schemaRoutes.get(API.HOME_PAGE_TRENDING_GAMES_SCHEMA, (req, res) => {
-  homepageTrendingGamesSchema(req, res);
 });
 
 export default schemaRoutes;
