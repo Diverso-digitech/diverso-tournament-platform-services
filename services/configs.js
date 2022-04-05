e445b63104033f97a35a9bdfe689ddfa9a94077d
Prakhar Shrivastava <prakharshrivastava971@gmail.com>
2022-04-05 21:46:42 +0530

    firebase keys removed from private config

--- services/configs.js
+++ services/configs.js
@@ -12,22 +12,7 @@ function getAppConfig(req, res) {
   );
 }
 function getPrivateConfig(req, res) {
-  formatResponse(
-    res,
-    200,
-    {
-      firebaseConfig: {
-        apiKey: "AIzaSyBFC6l-njCw3Xr1JIq_oyBH-1DsW0ZXqAQ",
-        authDomain: "diverso-gaming-platform.firebaseapp.com",
-        projectId: "diverso-gaming-platform",
-        storageBucket: "diverso-gaming-platform.appspot.com",
-        messagingSenderId: "488170373720",
-        appId: "1:488170373720:web:915abe2aea36400007402d",
-        measurementId: "G-2TSYJZXXWM",
-      },
-    },
-    false
-  );
+  formatResponse(res, 200, {}, false);
 }
 function getEndpointsConfig(req, res) {
   formatResponse(
