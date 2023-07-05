37e8e99c170c3391dc84496a23839824e466ed37
Shashank Malviya <shashankm.malviya@gmail.com>
2023-07-05 18:48:06 +0530

    Update env.js

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
-export const dbString = "mongodb://localhost:27017/diverso";
+export const dbString = "mongodb+srv://sahil:sahilmalviya@cluster0.hhowf26.mongodb.net/shashankdbtest?retryWrites=true&w=majority";
 export const AUTH_SECRET = "MY_AUTH_SECRET";
